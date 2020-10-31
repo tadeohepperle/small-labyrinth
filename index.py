e13b50abceaa4b16c4e739ccc7e8f35f7f6756c1
Tadeo <tadeo@do-mix.de>
2020-10-31 17:03:07 +0100

    xy

--- index.py
+++ index.py
@@ -1,20 +1,31 @@
 from PIL import Image
+# pip install Pillow
 
 
-def imageFilePathToPixels(filepath):
-    img = Image.open(filepath)
-    arr = []
-    x = img.size[0]
-    y = img.size[1]
-    for xi in range(x):
-        arr.append([])
-        for yi in range(y):
-            pix = img.getpixel((xi, yi))
-            (r, g, b, a) = pix
-            pix_as_num = 1 if r+g+b > 700 else 0
-            arr[xi].append(pix_as_num)
-    return arr
+class PixelMap:
+    x = 0
+    y = 0
+    map = [[]]
+    img = None
+    entryCoordinate = (1, 1)
+
+    def __init__(self, imagePath, entryCoordinate):
+        self.img = Image.open(imagePath)
+        self.convertImageToMap()
+
+    def convertImageToMap(self):
+        arr = []
+        x = self.img.size[1]
+        y = self.img.size[0]
+        for xi in range(x):
+            arr.append([])
+            for yi in range(y):
+                pix = self.img.getpixel((xi, yi))
+                (r, g, b, a) = pix
+                pix_as_num = 1 if r+g+b > 700 else 0
+                arr[xi].append(pix_as_num)
+        self.map = arr
 
 
-pix_arr = imageFilePathToPixels("labyrinth.png")
-print(pix_arr)
+pixelMap = PixelMap("labyrinth.png", (1, 1))
+print(pixelMap.map)
